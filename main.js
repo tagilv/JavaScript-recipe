9e386cadd017b425d39f78173d8a34f119cfc82d
Viktor Tagil <viktor.tagil@gmail.com>
2022-10-13 22:07:22 +0200

    2ndfilter

--- main.js
+++ main.js
@@ -59,6 +59,11 @@ function creatingRecipes(recipes) {
     cardText.classList.add("invisable");
     cardText.innerText = "Add short description here";
     divCard.appendChild(cardText);
+
+    let likeText = document.createElement("p");
+    likeText.classList.add("card-text");
+    likeText.innerText = `${recipes[i].likes} Likes`;
+    divCard.appendChild(likeText);
   }
 }
 
@@ -73,29 +78,34 @@ function calculatingTotalNeededIngredients(recipes) {
   return neededIngredientsArray;
 }
 
-// function creatingDropdown(recipes) {}
-
-// cretae input
-// add events to those inouts
-// filter function the values of the checkboxes that have been cliked
-
-// inputs values, max value
-
 function addEvents(recipes) {
   let cards = document.querySelectorAll(".card");
   for (let i = 0; i < cards.length; i++) {
     divCardTextTargets = document.querySelectorAll(".card")[i];
-
     divCardTextTargets.addEventListener("click", (e) => {
       unhideText(e);
     });
   }
-  // Uncomment to test
+
   let dropdown = document.getElementById("ingredientsDropdown");
+  console.log(dropdown);
+  const value = filterByDropdown(recipes);
+  // We dont need const value here right?
   dropdown.addEventListener("change", (e) => {
-    // console.log("# of recipe dropdown works");
-    const value = filterByDropdown(recipes);
     console.log("value>>", value);
+  });
+
+  let likesFilter = document.querySelectorAll(".check-button", "input:checked");
+  // console.log("likesFilter>>", likesFilter);
+  likesFilter.forEach((item) => {
+    item.addEventListener("change", (e) => {
+      // console.log(likesFilter);
+      // Call filtering function to move to filtering:
+      // console.log(
+      //   Array.from(document.querySelectorAll(".check-button", "input:checked"))
+      // );
+      filterByDropdown(recipes);
+    });
   });
 }
 
@@ -106,152 +116,25 @@ function unhideText(e) {
 }
 
 function filterByDropdown(recipes) {
-  // console.log("I am filtering by dropdown");
+  //Converting the node list to an array:
+  const likesValuesArray = Array.from(
+    document.querySelectorAll("input[type='checkbox']:checked")
+  );
+  console.log("likesValuesArray>>", likesValuesArray);
+
   const dropDownValue = document.getElementById("ingredientsDropdown").value;
   console.log("dropDownValue>>", dropDownValue);
   if (dropDownValue === "allIngredientsSelected") {
+    // && Andra tom
     creatingRecipes(recipes);
   } else {
     const dropDownValueInt = parseInt(dropDownValue);
-    // console.log("dropDownValueInt>>", dropDownValueInt);
-    // console.log("recipes>>", recipes);
-    // const filteredOptions = recipes.filter();
     let filteredRecepieOptions = recipes.filter((recipe) => {
       const count = recipe.missedIngredientCount + recipe.usedIngredientCount;
       return count === dropDownValueInt;
     });
     //Skriv .filter igen har
-    // console.log("filteredOptions>>", filteredOptions);
+    // Include the second filtering in this function?
     creatingRecipes(filteredRecepieOptions);
   }
-  // console.log(filteredOptions);
 }
-
-//To dos
-//1. Itterate over the images X
-//*. Use grid to make it responsive X
-//2. Text that shows and hiddes
-//3. Read Ajax chapter
-//4. Design
-//5. AJAX AND Asynchronous calls
-//*  Add Spanish food as default food
-//*  Pagination?
-// Question: function() in arguments but not when you include it as a argument in calling it?
-// Friday fetch
-// XX.json() that is a method that transfor XX to a json file (so JavaScript can read it
-
-// // Function declaration
-// function name(params) {}
-// // function expression
-// const myFunction = function () {};
-
-// const arrowFnc = () => {};
-
-// WANT TO REMOVE DUPLICATES AND SORT TO USE FOR THE DROPDOWN LIST:
-
-// function filterByDropdown(recipes) {
-//   // console.log("I am filtering by dropdown");
-//   const dropDownValue = document.getElementById("ingredientsDropdown").value;
-//   console.log("dropDownValue>>", dropDownValue);
-//   if (dropDownValue === "all") {
-//     creatingRecipes(recipes);
-//   } else {
-//     const dropDownValueInt = parseInt(dropDownValue);
-//     // console.log("dropDownValueInt>>", dropDownValueInt);
-//     // console.log("recipes>>", recipes);
-//     const filteredOptions = recipes.filter((recipe) => {
-//       const count = recipe.missedIngredientCount + recipe.usedIngredientCount;
-//       return count === dropDownValueInt;
-//     });
-//     console.log("filteredOptions>>", filteredOptions);
-//     creatingRecipes(filteredOptions);
-//   }
-//   // console.log(filteredOptions);
-// }
-
-// // Anvand denna
-// const ALL_INGREDIENTS_SELECTED = -1;
-// function filterByDropdown(recipes) {
-//   // console.log("I am filtering by dropdown");
-//   const dropDownValue = document.getElementById("ingredientsDropdown").value;
-//   console.log("dropDownValue>>", dropDownValue);
-//   // if (dropDownValue === "-1") {
-//   //   creatingRecipes(recipes);
-//   // } else {
-//   const dropDownValueInt = parseInt(dropDownValue);
-//   // console.log("dropDownValueInt>>", dropDownValueInt);
-//   // console.log("recipes>>", recipes);
-//   const filteredOptions = recipes.filter((recipe) => {
-//     const count = recipe.missedIngredientCount + recipe.usedIngredientCount;
-//     return (
-//       dropDownValueInt === ALL_INGREDIENTS_SELECTED ||
-//       count === dropDownValueInt
-//     );
-//   });
-
-//   console.log("filteredOptions>>", filteredOptions);
-//   creatingRecipes(filteredOptions);
-//   // }
-//   // console.log(filteredOptions);
-// }
-
-// function filterByDropdown(recipes) {
-//   // console.log("I am filtering by dropdown");
-//   const dropDownValue = document.getElementById("ingredientsDropdown").value;
-//   console.log("dropDownValue>>", dropDownValue);
-//   if (dropDownValue === "allIngredientsSelected") {
-//     creatingRecipes(recipes);
-//   } else {
-//     const dropDownValueInt = parseInt(dropDownValue);
-//     // console.log("dropDownValueInt>>", dropDownValueInt);
-//     // console.log("recipes>>", recipes);
-//     const filteredOptions = recipes.filter((recipe) => {
-//       const count = recipe.missedIngredientCount + recipe.usedIngredientCount;
-//       return count === dropDownValueInt;
-//     });
-//     console.log("filteredOptions>>", filteredOptions);
-//     creatingRecipes(filteredOptions);
-//   }
-//   // console.log(filteredOptions);
-// }
-
-// // FILTER WIHTOUT FILTER FUNCTION
-
-// function generalFilter(list, filtering) {
-//   let filteredOptions = [];
-//   for (const item of list) {
-//     let shouldBeInFilteredOptions = filtering(item);
-//     if (shouldBeInFilteredOptions) {
-//       filteredOptions.push(item);
-//     }
-//   }
-//   return filteredOptions;
-// }
-// function filterValue(recipe, dropDownValueInt) {
-//   const count = recipe.missedIngredientCount + recipe.usedIngredientCount;
-//   return count === dropDownValueInt;
-// }
-
-// function filterByDropdown(recipes) {
-//   // console.log("I am filtering by dropdown");
-//   const dropDownValue = document.getElementById("ingredientsDropdown").value;
-//   console.log("dropDownValue>>", dropDownValue);
-//   if (dropDownValue === "allIngredientsSelected") {
-//     creatingRecipes(recipes);
-//   } else {
-//     const dropDownValueInt = parseInt(dropDownValue);
-//     // console.log("dropDownValueInt>>", dropDownValueInt);
-//     // console.log("recipes>>", recipes);
-//     // const filteredOptions = recipes.filter();
-
-//     let filteredRecepieOptions = generalFilter(recipes, (recipe) =>
-//       filterValue(recipe, dropDownValueInt)
-//     );
-
-//     // console.log("filteredOptions>>", filteredOptions);
-//     creatingRecipes(filteredRecepieOptions);
-//   }
-//   // console.log(filteredOptions);
-// }
-
-// // borja med filterByDropdown
